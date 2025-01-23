7b5aa2d30eba7dcc33027c5af05d825d70b15076
Eiko <worst.mirari@gmail.com>
2025-01-23 10:35:09 -0800

    version bump to 4.0.9

--- randomizer/Randomizer.cs
+++ randomizer/Randomizer.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 public static class Randomizer
 {
-    public static string VERSION = "4.0.8";
+    public static string VERSION = "4.0.9";
     public static void initialize()
     {
         try {
